147a936d0b08cae3a3df5309d0b6740142b1d336
justin <jebusiness723@gmail.com>
2024-03-30 18:38:08 -0700

    Links - find mailto links and add an alias

--- script.js
+++ script.js
@@ -34,17 +34,17 @@ const UTM = {
     // Function to check if URL contains UTM parameters
     const urlParams = new URLSearchParams(window.location.search)
     // Check if all UTM parameters are present
-    console.log('test2')
     const utmValues = {
       'source': urlParams.get('utm_source'),
       'medium': urlParams.get('utm_medium'),
       'campaign': urlParams.get('utm_campaign'),
     }
     if (utmValues.source && utmValues.medium && utmValues.campaign) {
-      console.log('Source: ' + utmValues.source, 'Medium: ' + utmValues.medium, 'Campaign: ', utmValues.campaign)
+      // console.log('Source: ' + utmValues.source, 'Medium: ' + utmValues.medium, 'Campaign: ', utmValues.campaign)
       if (utmValues.source === 'rehabpath' && utmValues.medium === 'referral' && utmValues.campaign === 'luxuryrehab') {
         Cookie.create('recoveryUTM', JSON.stringify(utmValues), 90)
         Form.insertHiddenFieldValues('#form-field-utm_campaign', utmValues.campaign)
+        Links.onLoad()
       }
     }
   },
@@ -59,11 +59,38 @@ const Form = {
   }
 }
 
+const Links = {
+  onLoad: function() {
+    $('body a').each(function() {
+      const url = $(this).attr('href')
+      console.log(url)
+      if (url.includes('mailto')) {
+        const email = url.replace('mailto:', '')
+        $(this).attr('href', Links.addAliasToEmail(email, '+recovery'))
+      }
+    })
+  },
+  addAliasToEmail: function (email, alias) {
+    console.log(email)
+    // Split the email address into local part and domain part
+    const parts = email.split('@')
+    // Check if the email is in valid format
+    if (parts.length !== 2) {
+      console.log('Invalid email format.')
+      return
+    }
+    // Insert the alias just before the domain part
+    const modifiedEmail = parts[0] + alias + '@' + parts[1]
+    return 'mailto:' + modifiedEmail
+  },
+}
+
 UTM.onLoad()
 const cookie_recoveryUTM = Cookie.read('recoveryUTM')
 if (cookie_recoveryUTM) {
   const utmValues = JSON.parse(cookie_recoveryUTM)
-  Form.insertHiddenFieldValues('#form-field-utm_campaign', utmValues.campaign)    
+  Form.insertHiddenFieldValues('#form-field-utm_campaign', utmValues.campaign)
+  Links.onLoad()
 }
 })(jQuery)
 // });
